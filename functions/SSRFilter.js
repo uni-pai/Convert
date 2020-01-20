b0478a3ae450ebac8fe2c5955fc1ce6aea030f55
珅 <ys06000@ly.com>
2020-01-20 14:21:42 +0800

    换成require

--- functions/SSRFilter.js
+++ functions/SSRFilter.js
@@ -3,6 +3,7 @@ const atob = require('atob');
 const btoa = require('btoa');
 const isUrl = require('is-url');
 const URLSafeBase64 = require('urlsafe-base64');
+const ssr = require('./addin/ssr');
 
 exports.handler = function (event, context, callback) {
   const {
@@ -31,7 +32,7 @@ exports.handler = function (event, context, callback) {
       //#region 支持协议过滤
       const filteredLinks = links.filter(link => {
         // Only support ssr now
-        if (link.startsWith('ss://')) return false;
+        if (link.startsWith('ss://')) return true;
         if (link.startsWith('ssr://')) return true;
         return false;
       });
@@ -50,82 +51,83 @@ exports.handler = function (event, context, callback) {
       const ssrInfos = new Array();
       const ssrLinks = new Array();
       filteredLinks.forEach(link => {
-        let host, port, protocol, method, obfs, base64password, password;
-        let base64obfsparam, obfsparam, base64protoparam, protoparam, base64remarks, remarks, base64group, group, udpport, uot;
-        let encodedStr = link.replace(/ssr:\/\//, "");
-        const decodedStr = URLSafeBase64.decode(encodedStr).toString();
-        const requiredParams = decodedStr.split(':');
-        if (requiredParams.length != 6) {
-          return true;
-        }
-        host = requiredParams[0];
-        port = requiredParams[1];
-        protocol = requiredParams[2];
-        method = requiredParams[3];
-        obfs = requiredParams[4];
-        const tempGroup = requiredParams[5].split('/?')
-        base64password = tempGroup[0];
-        password = URLSafeBase64.decode(base64password).toString();
+        var result = ssr.analyseSSR(link);
+        // let host, port, protocol, method, obfs, base64password, password;
+        // let base64obfsparam, obfsparam, base64protoparam, protoparam, base64remarks, remarks, base64group, group, udpport, uot;
+        // let encodedStr = link.replace(/ssr:\/\//, "");
+        // const decodedStr = URLSafeBase64.decode(encodedStr).toString();
+        // const requiredParams = decodedStr.split(':');
+        // if (requiredParams.length != 6) {
+        //   return true;
+        // }
+        // host = requiredParams[0];
+        // port = requiredParams[1];
+        // protocol = requiredParams[2];
+        // method = requiredParams[3];
+        // obfs = requiredParams[4];
+        // const tempGroup = requiredParams[5].split('/?')
+        // base64password = tempGroup[0];
+        // password = URLSafeBase64.decode(base64password).toString();
 
-        if (tempGroup.length > 1) {
-          const optionalParams = tempGroup[1];
-          optionalParams.split('&').forEach(param => {
-            const temp = param.split('=');
-            let key = temp[0],
-              value;
-            if (temp.length > 1) {
-              value = temp[1];
-            }
+        // if (tempGroup.length > 1) {
+        //   const optionalParams = tempGroup[1];
+        //   optionalParams.split('&').forEach(param => {
+        //     const temp = param.split('=');
+        //     let key = temp[0],
+        //       value;
+        //     if (temp.length > 1) {
+        //       value = temp[1];
+        //     }
 
-            if (value) {
-              switch (key) {
-                case 'obfsparam':
-                  base64obfsparam = value;
-                  obfsparam = URLSafeBase64.decode(base64obfsparam).toString();
-                  break;
-                case 'protoparam':
-                  base64protoparam = value;
-                  protoparam = URLSafeBase64.decode(base64protoparam).toString();
-                  break;
-                case 'remarks':
-                  base64remarks = value;
-                  remarks = URLSafeBase64.decode(base64remarks).toString();
-                  break;
-                case 'group':
-                  base64group = value;
-                  group = URLSafeBase64.decode(base64group).toString();
-                  break;
-                case 'udpport':
-                  udpport = value;
-                  break;
-                case 'uot':
-                  uot = value;
-                  break;
-              }
-            }
-          })
-        }
+        //     if (value) {
+        //       switch (key) {
+        //         case 'obfsparam':
+        //           base64obfsparam = value;
+        //           obfsparam = URLSafeBase64.decode(base64obfsparam).toString();
+        //           break;
+        //         case 'protoparam':
+        //           base64protoparam = value;
+        //           protoparam = URLSafeBase64.decode(base64protoparam).toString();
+        //           break;
+        //         case 'remarks':
+        //           base64remarks = value;
+        //           remarks = URLSafeBase64.decode(base64remarks).toString();
+        //           break;
+        //         case 'group':
+        //           base64group = value;
+        //           group = URLSafeBase64.decode(base64group).toString();
+        //           break;
+        //         case 'udpport':
+        //           udpport = value;
+        //           break;
+        //         case 'uot':
+        //           uot = value;
+        //           break;
+        //       }
+        //     }
+        //   })
+        // }
 
-        result = {
-          type: 'ss/ssr',
-          host,
-          port,
-          protocol,
-          method,
-          obfs,
-          base64password,
-          password,
-          base64obfsparam,
-          obfsparam,
-          base64protoparam,
-          protoparam,
-          base64remarks,
-          remarks,
-          base64group,
-          group,
-          udpport,
-          uot
-        }
+        // result = {
+        //   type: 'ss/ssr',
+        //   host,
+        //   port,
+        //   protocol,
+        //   method,
+        //   obfs,
+        //   base64password,
+        //   password,
+        //   base64obfsparam,
+        //   obfsparam,
+        //   base64protoparam,
+        //   protoparam,
+        //   base64remarks,
+        //   remarks,
+        //   base64group,
+        //   group,
+        //   udpport,
+        //   uot
+        // }
         //#region 协议根据名称进行过滤
 
         if (filter && filter != "" && !new RegExp(filter).test(result.remarks)) {
